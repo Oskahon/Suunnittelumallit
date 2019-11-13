e11e79644938534a5156a62a7fa1a2fccb0a84de
Oskari Ahonen <ahonenoskari@gmail.com>
2019-11-13 11:05:45 +0200

    Template tehtävä

---
+++ src/template/DiceGame.java
@@ -0,0 +1,65 @@
+package template;
+
+import java.util.Random;
+
+public class DiceGame extends Game{
+	
+	private DiceGame() {};
+	
+	Random rand;
+	int[] rolls;
+	int plays;
+	int highestRoller;
+	int ties;
+	
+	private static final Game INSTANCE = new DiceGame();
+	
+	public static Game getInstance() {
+		return INSTANCE;
+	}
+	
+	@Override
+	public void initializeGame() {
+		rand = new Random();
+		rolls = new int[super.playersCount];
+		highestRoller = 0;
+		ties = 0;
+		for (int i = 0; i < super.playersCount; i++) {
+			rolls[i] = 0;
+		}
+	}
+
+	@Override
+	public void makePlay(int player) {
+		rolls[player] = rand.nextInt(10)+1;
+		plays++;
+		System.out.println("player "+(player+1)+": "+rolls[player]);
+	}
+
+	@Override
+	public boolean endOfGame() {
+		if (plays == super.playersCount) {
+			int highroll = 0;
+			for (int i = 0; i < super.playersCount;i++) {
+				if (rolls[i]>highroll) {
+					highroll = rolls[i];
+					highestRoller = i+1;
+				} else if (rolls[i] == highroll) {
+					ties++;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void printWinner() {
+		if (ties == 0) {
+		System.out.println("The winner is player "+highestRoller+"!");
+		} else {
+			System.out.println("It's a tie!");
+		}
+	}
+
+}
