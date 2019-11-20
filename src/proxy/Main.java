052f57d91adebafbad52304aba817b78e5c3464a
Oskari Ahonen <ahonenoskari@gmail.com>
2019-11-20 10:38:25 +0200

    Proxy tehtävä

---
+++ src/proxy/Main.java
@@ -0,0 +1,33 @@
+package proxy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		List<Image> kuvat = new ArrayList<>();
+		
+		Image kuva1 = new ProxyImage("kuva1");
+		Image kuva2 = new ProxyImage("kuva2");
+		Image kuva3 = new ProxyImage("kuva3");
+		Image kuva4 = new ProxyImage("kuva4");
+		
+		kuvat.add(kuva1);
+		kuvat.add(kuva2);
+		kuvat.add(kuva3);
+		kuvat.add(kuva4);
+		
+		for (Image i:kuvat) {
+			i.showData();
+		}
+		System.out.println();
+		
+		for(Image i:kuvat) {
+			i.displayImage();
+		}
+		
+	}
+
+}
